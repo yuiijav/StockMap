3ce1bdc8ecf196eb42e2f4a5fa7ed7e6bddfa180
yuiijav <fuku.utsuhei12@gmail.com>
2024-01-31 00:52:07 +0900

    コメントアウトの追記と削除

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,21 +2,20 @@ package org.example;
 import java.io.*;
 import java.util.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Map<String, Integer> stocks = new HashMap<>();
         stocks.put("トイレットペーパー", 3);
         stocks.put("キッチンペーパー", 4);
         stocks.put("ティッシュペーパー", 8);
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
+        //Mapの中身を表示させる
         for (String key : stocks.keySet()){
             int value = stocks.get(key);
             System.out.println(key + "の在庫数は" + value + "個です");
         }
 
+        //特定の消耗品に対してだけ在庫数を表示させる際に例外を設定
         System.out.println("在庫数が知りたい消耗品の名前を入力してください");
         try{
             String item = new java.util.Scanner(System.in).nextLine();
